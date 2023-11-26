dea96bdbb3d2b4c7a0eb6e3cbe0f7ccd23d09f9b
Johannes Becker <jbecker@student.tugraz.at>
2023-11-26 17:46:00 +0100

    linting updates

--- setup.py
+++ setup.py
@@ -15,4 +15,15 @@ setup(
     author_email="johannes@becker.computer",
     url="https://github.com/hydrogen-1/twitchchat-wss",
     install_requires=["websocket-client >= 1.6.4"],
+    classifiers=[
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Operating System :: OS Independent",
+    ],
+    extra_require={
+        "dev": ["twine>=4.0.2"]
+    },
+    python_requres=">=python3.9"
 )
